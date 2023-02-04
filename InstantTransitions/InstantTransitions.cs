1b4851b099f69b56515bce76211a154b2a307d52
TwinBuilderOne <jackreynolds9128@gmail.com>
2023-02-04 10:19:37 -0600

    Remove preload triggers; stretch gate colliders

--- InstantTransitions/InstantTransitions.cs
+++ InstantTransitions/InstantTransitions.cs
@@ -6,17 +6,13 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace InstantTransitions;
 public class InstantTransitionsMod : Mod
 {
     private static InstantTransitionsMod? _instance;
 
-    public Dictionary<string, AsyncOperation> LoadingScenes { get; } = new();
-    public Dictionary<string, AsyncOperation> UnloadingScenes { get; } = new();
-
-    public float LoadCooldown { get; set; }
+    public bool LoadCooldown { get; set; }
 
     private List<ILHook>? ILHooks { get; set; }
 
@@ -43,32 +39,39 @@ public class InstantTransitionsMod : Mod
     {
         Log("Initializing");
 
-        On.GameManager.BeginSceneTransitionRoutine += GameManager_BeginSceneTransitionRoutine;
-        On.TransitionPoint.Awake += TransitionPoint_Awake;
-        On.SceneLoad.Begin += SceneLoad_Begin;
-
-        ModHooks.HeroUpdateHook += ModHooks_HeroUpdateHook;
-        UnityEngine.SceneManagement.SceneManager.activeSceneChanged += SceneManager_activeSceneChanged;
-
         ILHooks = new()
         {
-            /*
-             * IEnumerator GameManager::BeginSceneTransitionRoutine(...)
-             * {
-             *     ...
-             *     sceneLoad.ActivationComplete += () =>
-             *     {
-             *         // Hooks into this function
-             *     }
-             *     ...
-             * }
-             */
-            new ILHook(typeof(GameManager).GetNestedType("<>c__DisplayClass174_0", BindingFlags.NonPublic)
-                .GetMethod("<BeginSceneTransitionRoutine>b__2", BindingFlags.NonPublic | BindingFlags.Instance),
-                GameManager_BeginSceneTransitionRoutine_ActivationCompletedAnonymous)
+            // MoveNext() for HeroController::EnterScene()
+            new ILHook(typeof(HeroController).GetNestedType("<EnterScene>d__469", BindingFlags.NonPublic)
+                .GetMethod("MoveNext", BindingFlags.NonPublic | BindingFlags.Instance),
+                HeroController_EnterScene_StateMachineMoveNext)
         };
 
+        On.GameManager.BeginSceneTransitionRoutine += GameManager_BeginSceneTransitionRoutine;
+        On.TransitionPoint.Awake += TransitionPoint_Awake;
+        On.HeroController.EnterScene += HeroController_EnterScene;
+        On.TransitionPoint.OnTriggerEnter2D += TransitionPoint_OnTriggerEnter2D;
+
         Log("Initialized");
+    }
+
+    private void GameManager_BeginSceneTransitionRoutine_ActivationCompletedAnonymous(ILContext il)
+    {
+        ILCursor cursor = new ILCursor(il).Goto(0);
+        cursor.RemoveRange(4);
+    }
+
+    private void HeroController_EnterScene_StateMachineMoveNext(ILContext il)
+    {
+        ILCursor cursor = new ILCursor(il).Goto(0);
+
+        while (cursor.TryGotoNext(
+            i => i.MatchLdloc(1),
+            i => i.MatchLdfld<HeroController>("gm"),
+            i => i.MatchCallvirt<GameManager>(nameof(GameManager.FadeSceneIn))))
+        {
+            cursor.RemoveRange(3);
+        }
     }
 
     private IEnumerator GameManager_BeginSceneTransitionRoutine(On.GameManager.orig_BeginSceneTransitionRoutine orig, GameManager self, GameManager.SceneLoadInfo info)
@@ -86,137 +89,61 @@ public class InstantTransitionsMod : Mod
 
     private void TransitionPoint_Awake(On.TransitionPoint.orig_Awake orig, TransitionPoint self)
     {
-        void SetupTrigger(bool unload)
+        if (self.targetScene != null && self.targetScene != "" && !self.isADoor)
         {
-            GameObject preloadTrigger = new($"Preload Trigger ({(unload ? "Unload" : "Load")})");
-            preloadTrigger.layer = 13; // Hero Detector
-            preloadTrigger.transform.parent = self.gameObject.transform;
-            preloadTrigger.transform.localPosition = Vector3.zero;
-
-            TransitionPreload tp = preloadTrigger.AddComponent<TransitionPreload>();
-            tp.Unload = unload;
-        }
-
-        if (self.targetScene != null && self.targetScene != "")
-        {
-            SetupTrigger(false);
-            SetupTrigger(true);
+            self.gameObject.AddComponent<TransitionStretch>();
         }
 
         orig(self);
     }
 
-    private void SceneLoad_Begin(On.SceneLoad.orig_Begin orig, SceneLoad self)
+    private IEnumerator HeroController_EnterScene(On.HeroController.orig_EnterScene orig, HeroController self, TransitionPoint enterGate, float delayBeforeEnter)
     {
-        // Heavily stripped-down version of SceneLoad::BeginCoroutine()
-        IEnumerator Coroutine(SceneLoad self, MonoBehaviour runner)
+        Time.timeScale = 3;
+
+        LoadCooldown = true;
+        List<WaitForSeconds> wait = new();
+
+        IEnumerator enumerator = orig(self, enterGate, delayBeforeEnter);
+
+        while (enumerator.MoveNext())
         {
-            void InvokeEvent(string name)
+            var item = enumerator.Current;
+            /*
+            if (item is WaitForSeconds w)
             {
-                ((MulticastDelegate)typeof(SceneLoad).GetField(name, BindingFlags.NonPublic | BindingFlags.Instance).GetValue(self))?.DynamicInvoke();
-            }
+                wait.Add(w);
 
-            Log($"Entering {self.TargetSceneName}");
-
-            LoadCooldown = 1f;
-
-            SceneAdditiveLoadConditional.loadInSequence = true;
-            yield return runner.StartCoroutine(ScenePreloader.FinishPendingOperations());
-
-            Scene scene = UnityEngine.SceneManagement.SceneManager.GetSceneByName(self.TargetSceneName);
-
-            if (scene.isLoaded)
-            {
-                Log($"Scene {self.TargetSceneName} already loaded");
-
-                InvokeEvent(nameof(SceneLoad.FetchComplete));
-                InvokeEvent(nameof(SceneLoad.WillActivate));
+                yield return null;
             }
             else
             {
-                AsyncOperation operation;
-
-                if (LoadingScenes.TryGetValue(self.TargetSceneName, out AsyncOperation op))
-                {
-                    Log($"Scene {self.TargetSceneName} successfully preloaded");
-                    LoadingScenes.Remove(self.TargetSceneName);
-                    operation = op;
-                }
-                else
-                {
-                    Log($"Scene {self.TargetSceneName} did not preload");
-                    operation = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(self.TargetSceneName);
-                }
-
-                while (operation.progress < 0.9f)
-                {
-                    yield return null;
-                }
-
-                InvokeEvent(nameof(SceneLoad.FetchComplete));
-                InvokeEvent(nameof(SceneLoad.WillActivate));
-
-                operation.allowSceneActivation = true;
-                yield return operation;
-
-                scene = UnityEngine.SceneManagement.SceneManager.GetSceneByName(self.TargetSceneName);
+                yield return item;
             }
-
-            foreach (GameObject go in UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects())
-            {
-                go.SetActive(false);
-            }
-
-            UnityEngine.SceneManagement.SceneManager.SetActiveScene(scene);
-
-            foreach (GameObject go in scene.GetRootGameObjects())
-            {
-                go.SetActive(true);
-            }
-
-            InvokeEvent(nameof(SceneLoad.ActivationComplete));
-
-            if (self.IsUnloadAssetsRequired) yield return Resources.UnloadUnusedAssets();
-            if (self.IsGarbageCollectRequired) GCManager.Collect();
-
-            InvokeEvent(nameof(SceneLoad.Complete));
-            yield return null;
-            InvokeEvent(nameof(SceneLoad.StartCalled));
-
-            if (SceneAdditiveLoadConditional.ShouldLoadBoss)
-            {
-                yield return runner.StartCoroutine(SceneAdditiveLoadConditional.LoadAll());
-                InvokeEvent(nameof(SceneLoad.BossLoaded));
-                GameManager.instance?.LoadedBoss();
-            }
-
-            ScenePreloader.Cleanup();
-            InvokeEvent(nameof(SceneLoad.Finish));
-
-            Log($"Finished entering {self.TargetSceneName}");
+            */
+            yield return item;
         }
 
-        if (LoadCooldown > 0) return;
+        Time.timeScale = 1;
+        LoadCooldown = false;
 
-        MonoBehaviour monoBehaviour = (MonoBehaviour)typeof(SceneLoad).GetField("runner", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(self);
+        // GameManager.instance.StartCoroutine(WaitCoroutine());
 
-        monoBehaviour.StartCoroutine(Coroutine(self, monoBehaviour));
+        IEnumerator WaitCoroutine()
+        {
+            foreach (WaitForSeconds w in wait)
+            {
+                yield return w;
+            }
+
+            LoadCooldown = false;
+        }
     }
 
-    private void GameManager_BeginSceneTransitionRoutine_ActivationCompletedAnonymous(ILContext il)
+    private void TransitionPoint_OnTriggerEnter2D(On.TransitionPoint.orig_OnTriggerEnter2D orig, TransitionPoint self, Collider2D movingObj)
     {
-        ILCursor cursor = new ILCursor(il).Goto(0);
-        cursor.RemoveRange(4);
-    }
+        if (LoadCooldown) return;
 
-    private void ModHooks_HeroUpdateHook()
-    {
-        LoadCooldown -= Time.deltaTime;
-        if (LoadCooldown < 0) LoadCooldown = 0;
-    }
-
-    private void SceneManager_activeSceneChanged(Scene arg0, Scene arg1)
-    {
-        Log($"Scene changed from {arg0.name} to {arg1.name}");
+        orig(self, movingObj);
     }
 }
