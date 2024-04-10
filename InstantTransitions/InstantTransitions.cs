f81dda6875cc7fed5574929cb0de906d7d833d33
TwinBuilderOne <jackreynolds9128@gmail.com>
2024-04-09 19:24:35 -0500

    Patched SceneLoad, initializes successfully

--- InstantTransitions/InstantTransitions.cs
+++ InstantTransitions/InstantTransitions.cs
@@ -2,8 +2,11 @@ using Modding;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using Mono.Cecil;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
+using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
@@ -12,6 +15,13 @@ namespace InstantTransitions;
 public class InstantTransitionsMod : Mod
 {
     private static InstantTransitionsMod? _instance;
+
+    private static List<string> ILGetPreloadedScenes()
+    {
+        return Instance.preloadedScenes;
+    }
+
+    private List<ILHook> hooks = [];
 
     private List<string> preloadedScenes = [];
 
@@ -38,27 +48,39 @@ public class InstantTransitionsMod : Mod
     {
         Log("Begin Initialization");
         
+        ModHooks.BeforeSceneLoadHook += BeforeSceneLoadHook;
+        
         On.GameManager.OnNextLevelReady += GameManagerOnOnNextLevelReady;
-        On.HeroController.HeroJump += HeroControllerOnHeroJump;
         
         IL.GameManager.FindTransitionPoint += GameManagerOnFindTransitionPoint;
+        
+        hooks.Add(new ILHook(
+            typeof(SceneLoad)
+                    .GetNestedType("<BeginRoutine>d__35", BindingFlags.NonPublic)
+                    .GetMethod("MoveNext", BindingFlags.NonPublic | BindingFlags.Instance), 
+            SceneLoadOnBeginRoutine));
 
         Log("End Initialization");
+    }
+
+    private string BeforeSceneLoadHook(string sceneName)
+    {
+        foreach (string scene in preloadedScenes)
+        {
+            if (scene != sceneName) Unload(scene);
+        }
+        
+        return sceneName;
     }
 
     private void GameManagerOnOnNextLevelReady(On.GameManager.orig_OnNextLevelReady orig, GameManager self)
     {
         orig(self);
         if (self.IsNonGameplayScene()) return;
+
+        preloadedScenes.Remove(UnitySceneManager.GetActiveScene().name);
         
         Preload("Crossroads_15");
-    }
-
-    private void HeroControllerOnHeroJump(On.HeroController.orig_HeroJump orig, HeroController self)
-    {
-        orig(self);
-        
-        // Preload("Crossroads_15");
     }
     
     private void GameManagerOnFindTransitionPoint(ILContext il)
@@ -72,9 +94,86 @@ public class InstantTransitionsMod : Mod
         cursor.Emit(OpCodes.Ldloc_2);
         cursor.Emit(OpCodes.Call, typeof(TransitionPoint)
             .GetProperty(nameof(TransitionPoint.isActiveAndEnabled),
-                BindingFlags.Public | BindingFlags.Instance)
-            !.GetGetMethod());
+                BindingFlags.Public | BindingFlags.Instance)!
+            .GetGetMethod());
         cursor.Emit(OpCodes.Brfalse_S, skipLabel);
+    }
+
+    private void SceneLoadOnBeginRoutine(ILContext il)
+    {
+        ILCursor cursor = new ILCursor(il).Goto(0);
+
+        Type stateMachineType = typeof(SceneLoad)
+            .GetNestedType("<BeginRoutine>d__35", BindingFlags.NonPublic);
+
+        // Insert before AsyncOperation loadOperation = UnitySceneManager.LoadSceneAsync(...)
+        // if (InstantTransitionsMod.ILGetPreloadedScenes().Contains(targetSceneName))
+        // {
+        //     loadOperation = null;
+        //     goto <RecordEndTime(SceneLoad.Phases.Fetch)>;
+        // }
+        ILLabel skipFetchLabel;
+        ILLabel loadNormallyLabel;
+        FieldReference targetSceneNameField = null!;
+        FieldReference loadOperationField = null!;
+        cursor.GotoNext(MoveType.Before,
+            i => i.MatchLdloc(1),
+            i => i.MatchLdcI4(1),
+            i => i.MatchCallvirt<SceneLoad>("RecordEndTime"));
+        skipFetchLabel = cursor.MarkLabel();
+        cursor.Goto(0);
+        cursor.GotoNext(MoveType.Before,
+            i => i.MatchLdarg(0),
+            i => i.MatchLdloc(1),
+            i => i.MatchLdfld<SceneLoad>("targetSceneName")
+                 && i.MatchLdfld(out targetSceneNameField),
+            i => i.MatchLdcI4(1),
+            i => i.MatchCall<UnitySceneManager>("LoadSceneAsync"),
+            i => i.MatchStfld(stateMachineType, "<loadOperation>5__2")
+                 && i.MatchStfld(out loadOperationField));
+        loadNormallyLabel = cursor.MarkLabel();
+        cursor.Emit(OpCodes.Call, typeof(InstantTransitionsMod)
+            .GetMethod(nameof(ILGetPreloadedScenes), BindingFlags.NonPublic | BindingFlags.Static));
+        cursor.Emit(OpCodes.Ldloc_1);
+        cursor.Emit(OpCodes.Ldfld, targetSceneNameField);
+        cursor.Emit(OpCodes.Call, typeof(List<string>)
+            .GetMethod(nameof(List<string>.Contains), BindingFlags.Public | BindingFlags.Instance));
+        cursor.Emit(OpCodes.Brfalse_S, loadNormallyLabel);
+        cursor.Emit(OpCodes.Ldarg_0);
+        cursor.Emit(OpCodes.Ldnull);
+        cursor.Emit(OpCodes.Stfld, loadOperationField);
+        cursor.Emit(OpCodes.Br_S, skipFetchLabel);
+
+        // Replace loadOperation.allowSceneActivation = true with
+        // if (loadOperation == null)
+        // {
+        //     InstantTransitionsMod.Activate(targetSceneName);
+        // }
+        // else
+        // {
+        //     loadOperation.allowSceneActivation = true;
+        // }
+        ILLabel allowActivationLabel;
+        ILLabel yieldReturnLabel;
+        cursor.GotoNext(MoveType.After, 
+            i => i.MatchLdarg(0),
+            i => i.MatchLdfld(loadOperationField),
+            i => i.MatchLdcI4(1),
+            i => i.MatchCallvirt(typeof(AsyncOperation)
+                .GetProperty(nameof(AsyncOperation.allowSceneActivation), 
+                    BindingFlags.Public | BindingFlags.Instance)!
+                .GetSetMethod()));
+        yieldReturnLabel = cursor.MarkLabel();
+        cursor.Goto(cursor.Index - 4);
+        allowActivationLabel = cursor.MarkLabel();
+        cursor.Emit(OpCodes.Ldarg_0);
+        cursor.Emit(OpCodes.Ldfld, loadOperationField);
+        cursor.Emit(OpCodes.Brtrue_S, allowActivationLabel);
+        Log(cursor.Prev.Operand);
+        cursor.Emit(OpCodes.Ldfld, targetSceneNameField);
+        cursor.Emit(OpCodes.Call, typeof(InstantTransitionsMod)
+            .GetMethod(nameof(Activate), BindingFlags.Public | BindingFlags.Static));
+        cursor.Emit(OpCodes.Br_S, yieldReturnLabel);
     }
 
     private void Preload(string sceneName)
@@ -94,10 +193,18 @@ public class InstantTransitionsMod : Mod
 
     private void Unload(string sceneName)
     {
-        UnitySceneManager.UnloadSceneAsync(sceneName);
+        if (!preloadedScenes.Contains(sceneName)) return;
+        if (!UnitySceneManager.GetSceneByName(sceneName).isLoaded) return;
+        
+        AsyncOperation? op = UnitySceneManager.UnloadSceneAsync(sceneName);
+        if (op == null) return;
+        op.completed += _ =>
+        {
+            preloadedScenes.Remove(sceneName);
+        };
     }
 
-    private static void Activate(Scene scene)
+    public static void Activate(Scene scene)
     {
         foreach (GameObject go in scene.GetRootGameObjects())
         {
@@ -105,7 +212,7 @@ public class InstantTransitionsMod : Mod
         }
     }
     
-    private static void Deactivate(Scene scene)
+    public static void Deactivate(Scene scene)
     {
         foreach (GameObject go in scene.GetRootGameObjects())
         {
